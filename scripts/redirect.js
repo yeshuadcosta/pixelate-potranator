48362d1d080f645dbc7d5dbd4ccd4dc4a212e82e
Yeshua D'Costa <148765835+yeshuadcosta@users.noreply.github.com>
2024-03-18 16:00:45 +0000

    Corrected paths in CSS

--- scripts/redirect.js
+++ scripts/redirect.js
@@ -1,35 +1,35 @@
 let path='';
 
 document.querySelector("#space").addEventListener("click", () => {
-    path = "./assets/images/space.jpg";
+    path = "../assets/images/space.jpg";
     getInfo();
 });
 document.querySelector("#amazon").addEventListener("click", () => {
-    path = "./assets/images/amazon.jpg";
+    path = "../assets/images/amazon.jpg";
     getInfo();
 });
 document.querySelector("#everest").addEventListener("click", () => {
-    path = "./assets/images/everest.jpg";
+    path = "../assets/images/everest.jpg";
     getInfo();
 });
 document.querySelector("#desert").addEventListener("click", () => {
-    path = "./assets/images/desert.jpg";
+    path = "../assets/images/desert.jpg";
     getInfo();
 });
 document.querySelector("#city").addEventListener("click", () => {
-    path = "./assets/images/city.jpg";
+    path = "../assets/images/city.jpg";
     getInfo();
 });
 document.querySelector("#country").addEventListener("click", () => {
-    path = "./assets/images/country.jpg";
+    path = "../assets/images/country.jpg";
     getInfo();
 });
 document.querySelector("#home").addEventListener("click", () => {
-    path = "./assets/images/home.jpg";
+    path = "../assets/images/home.jpg";
     getInfo();
 });
 document.querySelector("#terrain").addEventListener("click", () => {
-    path = "./assets/images/terrain.jpg";
+    path = "../assets/images/terrain.jpg";
     getInfo();
 });
 
